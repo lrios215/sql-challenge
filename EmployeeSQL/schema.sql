5497f4868440b2b6e40a1e489afd3cb0d89d6703
lrios215 <70179261+lrios215@users.noreply.github.com>
2021-02-09 21:59:45 -0500

    File Updates

---
+++ EmployeeSQL/schema.sql
@@ -0,0 +1,42 @@
+﻿-- Exported from QuickDBD: https://www.quickdatabasediagrams.com/
+-- Link to schema: https://app.quickdatabasediagrams.com/#/d/3zpmKB
+
+
+--creating a table schema for all 6 CSV files
+CREATE TABLE departments (
+    dept_no VARCHAR,
+    dept_name VARCHAR(40)
+);
+
+CREATE TABLE dept_emp (
+    emp_no INTEGER,
+    dept_no VARCHAR
+);
+
+CREATE TABLE dept_manager (
+    dept_no VARCHAR,
+    emp_no INTEGER
+);
+
+CREATE TABLE employees (
+    emp_no INTEGER,
+    emp_title_id VARCHAR,
+    birth_date DATE,
+    first_name VARCHAR,
+    last_name VARCHAR,
+    sex VARCHAR(1),
+    hire_date DATE
+);
+
+CREATE TABLE salaries (
+    emp_no INTEGER,
+    salary INTEGER
+);
+
+CREATE TABLE titles (
+    title_id VARCHAR,
+    title VARCHAR
+);
+
+---importing and verifying csvs
+SELECT * FROM departments
